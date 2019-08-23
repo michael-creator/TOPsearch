44a056f283fadc5790fc93b7a8918c0ba5a3bd04
michael <mikelkarije@gmail.com>
2019-08-23 14:00:29 +0300

    this commits add gif to my angular app

---
+++ src/app/topsearch.spec.ts
@@ -0,0 +1,7 @@
+import { TOPsearch } from './topsearch';
+
+describe('TOPsearch', () => {
+  it('should create an instance', () => {
+    expect(new TOPsearch()).toBeTruthy();
+  });
+});
---
+++ src/app/topsearch.ts
@@ -0,0 +1,4 @@
+export class Topsearch {
+    id: number;
+    name: string;
+  }
